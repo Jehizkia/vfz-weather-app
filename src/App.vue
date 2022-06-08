62838f25a8da3a343574893e35e360fc51e9cd45
HiZ <jehizkiagaria@gmail.com>
2022-06-08 18:37:26 +0200

    Added: - Connection with openweathermaps api - User is able to add cities

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,94 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Lekker weertje hè</h1>
+
+    <weather-card
+        v-for="(weatherData, index) in weatherCasts"
+        :weather="weatherData"
+        :key="`weather-card-${index}`"
+    />
+
+    {{ errors }}
+    {{ cityName }}
+    <div>
+      <input v-model="cityName" @keyup.enter="addCity">
+      <button @click="addCity">
+        Add
+      </button>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios'
+import WeatherCard from '@/components/WeatherCard'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WeatherCard
+  },
+  data () {
+    return {
+      cityData: null,
+      weatherLookUp: null,
+      defaultCities: [
+        'Amsterdam',
+      ],
+      weatherCasts: [],
+      unit: 'metric',
+      cityName: '',
+      errors: [
+      ]
+    }
+  },
+  mounted () {
+    console.log(localStorage.getItem('defaultCities'))
+    // let localDefaultCities = localStorage.getItem('defaultCities')
+    // if (localDefaultCities) this.defaultCities = localDefaultCities
+
+    this.getDefaultWeather()
+  },
+  methods: {
+    async searchCity (city) {
+      return this.cityData = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=4d1d1428cb00886d5476c104d3ea1b54`)
+    },
+
+    async getWeatherByLatLon (lat, lon) {
+      return this.weatherLookUp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${this.unit}&appid=4d1d1428cb00886d5476c104d3ea1b54`)
+    },
+
+    async getWeatherByCityName (city) {
+      try {
+        let cityData = await this.searchCity(city)
+        return await this.getWeatherByLatLon(cityData.data[0].lat, cityData.data[0].lon)
+      } catch (e) {
+        this.errors.push(`Could not find city: ${city}`)
+      }
+    },
+
+     getDefaultWeather () {
+      for (const cityName of this.defaultCities) {
+        this.addCityToList(cityName)
+      }
+    },
+
+    async addCityToList(cityName) {
+      try {
+        let cityWeatherData = await this.getWeatherByCityName(cityName)
+        this.weatherCasts.push(cityWeatherData.data)
+        // localStorage.setItem('defaultCities', this.defaultCities)
+      } catch (e) {
+        this.errors.push(`Failed to add city: ${cityName}  to the list. Please check your input.`)
+      }
+    },
+
+    addCity() {
+      this.addCityToList(this.cityName)
+      this.cityName = ''
+    }
   }
 }
 </script>
---
+++ src/components/WeatherCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>
+      {{ weather.name }}
+    </h1>
+    <p>
+      {{ weather.main.temp}}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    weather: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
